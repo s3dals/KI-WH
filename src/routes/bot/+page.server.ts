08d4de9ee4922e9564c81041649f9fa246f45c3e
S3d-ALS <98984673+s3dals@users.noreply.github.com>
2024-03-10 21:24:42 +0100

    remove pupperteer

--- src/routes/bot/+page.server.ts
+++ src/routes/bot/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad, Actions  } from './$types';
-import puppeteer, { Browser } from '@cloudflare/puppeteer';
+// import puppeteer, { Browser } from '@cloudflare/puppeteer';
 import { get } from 'svelte/store';
 
 import { websiteStore } from '$lib/storage.ts';
@@ -7,37 +7,37 @@ import { websiteStore } from '$lib/storage.ts';
 export const load = (async () => {
 	const website = get(websiteStore);
 
-	if (website.url) {
-		const brwoser: Browser = await puppeteer.launch({ headless: true });
-		const page = await brwoser.newPage();
-		await page.goto(website.url);
-		try {
-			await page.click('[id=gdpr-banner-accept]');
-			// ...
-		  } catch (error) {
-			console.log("Cookies already accepted")
-		  }
+	// if (website.url) {
+	// 	const brwoser: Browser = await puppeteer.launch({ headless: true });
+	// 	const page = await brwoser.newPage();
+	// 	await page.goto(website.url);
+	// 	try {
+	// 		await page.click('[id=gdpr-banner-accept]');
+	// 		// ...
+	// 	  } catch (error) {
+	// 		console.log("Cookies already accepted")
+	// 	  }
 		
-		const siteData = await page.evaluate(() => {
-			const bookPods = Array.from(document.querySelectorAll('.aditem'));
-			const data = bookPods.map((book: any) => ({
-				title: book.querySelector('h2 a').innerText,
-				price: book.querySelector('.aditem-main--middle--price-shipping--price').innerText
-			}));
+	// 	const siteData = await page.evaluate(() => {
+	// 		const bookPods = Array.from(document.querySelectorAll('.aditem'));
+	// 		const data = bookPods.map((book: any) => ({
+	// 			title: book.querySelector('h2 a').innerText,
+	// 			price: book.querySelector('.aditem-main--middle--price-shipping--price').innerText
+	// 		}));
 	
-			return data;
-		});
+	// 		return data;
+	// 	});
 	
-		// // const page2 = await brwoser.newPage();
-		// // await page2.goto('https://bot.sannysoft.com');
-		// // await page2.screenshot({ path: 'bot.jpg'});
+	// 	// // const page2 = await brwoser.newPage();
+	// 	// // await page2.goto('https://bot.sannysoft.com');
+	// 	// // await page2.screenshot({ path: 'bot.jpg'});
 	
-		await brwoser.close();
-		return {
-			website,
-			siteData
-		};
-	}
+	// 	await brwoser.close();
+	// 	return {
+	// 		website,
+	// 		siteData
+	// 	};
+	// }
 
 
 	return {
