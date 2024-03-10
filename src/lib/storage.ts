ef06bc544c1cc355ba602038b977846d770cdd55
S3d-ALS <98984673+s3dals@users.noreply.github.com>
2024-03-10 18:50:12 +0100

    storage

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -2,35 +2,35 @@ import { localStorageStore } from "@skeletonlabs/skeleton";
 import type { Writable } from "svelte/store";
 import { bewerbungsDemo } from "./demo.ts";
 
-// type profile = {
-//     id: string;
-//     fullName: string;
-//     birth: string;
-//     job: string;
-//     employer: string;
-//     jobsince: string;
-//     hobbys: string;
-//     more: string;
+type profile = {
+    id: string;
+    fullName: string;
+    birth: string;
+    job: string;
+    employer: string;
+    jobsince: string;
+    hobbys: string;
+    more: string;
 
-// };
+};
 
-// type bewerbungs = {
-//     uid: string;
-//     date: Date;
-//     fullName: string;
-//     address: string;
-//     additional: string;
-//     application: string;
+type bewerbungs = {
+    uid: string;
+    date: Date;
+    fullName: string;
+    address: string;
+    additional: string;
+    application: string;
 
-// };
+};
 
-// type settings = {
-//     id: string;
-//     apikey: Date;
-//     mode: string;
-//     lang: string;
+type settings = {
+    id: string;
+    apikey: Date;
+    mode: string;
+    lang: string;
 
-// };
+};
 
 type Website = {
     name: string;
@@ -39,6 +39,6 @@ type Website = {
 
 export const websiteStore: Writable<Website[]> = localStorageStore('searchSite', []);
 
-// export const bewerbungsStore: Writable<bewerbungs[]> = localStorageStore('bewerbungs', bewerbungsDemo);
-// export const profileStore: Writable<profile[]> = localStorageStore('profile', false);
-// export const settingsStore: Writable<settings[]> = localStorageStore('settings', false);
+export const bewerbungsStore: Writable<bewerbungs[]> = localStorageStore('bewerbungs', bewerbungsDemo);
+export const profileStore: Writable<profile[]> = localStorageStore('profile', false);
+export const settingsStore: Writable<settings[]> = localStorageStore('settings', false);
